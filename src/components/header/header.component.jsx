7d7b00135157f455101e1eb6b694b3806b974200
chandan126 <chandanbhat1265@gmail.com>
2021-04-05 11:24:33 +0200

    added cartIcon and cartDropDown component and added a toggle for the cartIcon'

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,9 +4,11 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils.js'
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import CartIcon from '../../components/cart-icon/cart-icon.component.jsx';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component.jsx';
 
 
-const Header = ({ currentUser }) =>(
+const Header = ({ currentUser,hidden }) =>(
     <div className='header'>
         <Link className='logo-container' to="/">
             <Logo className='logo'/>
@@ -23,13 +25,19 @@ const Header = ({ currentUser }) =>(
                 div className='option' onClick={() => auth.signOut()}> SIGN OUT </div>
                 : 
                 <Link className='option' to='/signin' > SIGN IN </Link>
-            }   
+            } 
+            <CartIcon />  
         </div>
+        {
+            hidden ? null : <CartDropdown />
+        }
+        
     </div>
 );
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header);
