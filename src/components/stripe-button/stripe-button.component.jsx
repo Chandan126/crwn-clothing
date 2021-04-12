b17d5d723299f2144e9045e200bb5bfb363649d1
chandan126 <chandanbhat1265@gmail.com>
2021-04-12 18:26:06 +0200

    implements that pulls the data from firestore and stores in shop page:

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51IdcabSGxfOr6cWStfvAgE7Hd3g6qDYzL5FrnaAlPJfrFyzd7BG9cQHj6H50sVhZfpoqbASHnXhwuIwvcSsRnTy500odTpGelg';
 
-const StripeCheckoutButton = ({price}) => {
-    const priceForStripe = price*100;
-    const publishableKey = 'pk_test_51IdcabSGxfOr6cWStfvAgE7Hd3g6qDYzL5FrnaAlPJfrFyzd7BG9cQHj6H50sVhZfpoqbASHnXhwuIwvcSsRnTy500odTpGelg';
-    
-    const onToken = token => {
-        console.log(token);
-        alert('Payment Successful');
-    }
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
 
-    return (
-        <StripeCheckout
-        label='Pay Now'
-        name='CRWN Clothing Ltd.'
-        billingAddress
-        shippingAddress
-        image='http://sendeyo.com/up/d/f3eb2117da'
-        description={`Your Total is $${price}`} 
-        amount={priceForStripe}
-        panelLabel='Pay Now'
-        token={onToken}
-        stripeKey={publishableKey}/>
-    );
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
 };
 
-export default  StripeCheckoutButton;
+export default StripeCheckoutButton;
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyCZO8X_4R2jRgberKC0yop_smeVT5YbICw",
-    authDomain: "crwn-db-10d4f.firebaseapp.com",
-    projectId: "crwn-db-10d4f",
-    storageBucket: "crwn-db-10d4f.appspot.com",
-    messagingSenderId: "1068541867640",
-    appId: "1:1068541867640:web:91a149b4f1f29c111f9f8d",
-    measurementId: "G-X3TX0N6PRW"
+  apiKey: "AIzaSyCZO8X_4R2jRgberKC0yop_smeVT5YbICw",
+  authDomain: "crwn-db-10d4f.firebaseapp.com",
+  projectId: "crwn-db-10d4f",
+  storageBucket: "crwn-db-10d4f.appspot.com",
+  messagingSenderId: "1068541867640",
+  appId: "1:1068541867640:web:91a149b4f1f29c111f9f8d",
+  measurementId: "G-X3TX0N6PRW"
 };
 
 firebase.initializeApp(config);
@@ -29,7 +29,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         displayName,
         email,
         createdAt,
-        ...additionalData
+        ...additionalData,
       });
     } catch (error) {
       console.log('error creating user', error.message);
@@ -37,6 +37,39 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   }
 
   return userRef;
+};
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach((obj) => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map((doc) => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items,
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
 };
 
 export const auth = firebase.auth();
